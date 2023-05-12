04141b92ad138608727b62e62ac9ce1148f54988
PatriciaFeio <patriciafeio@gmail.com>
2023-05-12 02:34:09 +0100

    feat: Add scroll to top button and scroll to section and refactor add active class to include both section and navbar item

--- js/app.js
+++ js/app.js
@@ -23,9 +23,10 @@
  * 
 */
 
+const navbar = document.querySelector('.navbar__menu');
 const navbarList = document.getElementById('navbar__list');
 const pageSections = document.querySelectorAll('section');
-
+const scrollToTopBtn = document.getElementById('scroll-to-top');
 
 /**
  * End Global Variables
@@ -34,53 +35,74 @@ const pageSections = document.querySelectorAll('section');
 */
 
 
+// Scroll to top of the page
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+};
+
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// build the nav
-
-
-//createNavBar();
-function createNavBar() {
-
+// build the navbar items from each section's data-nav attribute
+function buildNavBar() {
     for (const data of pageSections) {
-        //console.log(data.id)
-        const dataNav = data.dataset.nav;
         const navbarListItem = document.createElement('li');
-        navbarListItem.innerHTML += `<a class='menu__link' href='#${data.id}'>${dataNav}</a>`;
+        const sectionId = data.getAttribute('id');
+        const listItemInnerText = data.getAttribute('data-nav');
+        navbarListItem.innerHTML += `<a class='menu__link' data-section-id='${sectionId}' href='#'>${listItemInnerText}</a>`;
         navbarList.appendChild(navbarListItem);
     }
 };
 
+// Scroll to section when clicking the related navbar item
+function scrollToSection() {
+    const navbarListItem = document.querySelectorAll('.menu__link');
+    for (const item of navbarListItem) {
+        item.addEventListener('click', e => {
+            e.preventDefault();
+            const sectionToScrollId = item.getAttribute('data-section-id');
+            const sectionToScrollTo = document.getElementById(sectionToScrollId);
+            sectionToScrollTo.scrollIntoView({ behavior: "smooth" });
+        })
+    };
+};
 
-// Add class 'active' to section when near top of viewport
+// Add class 'active' to section and to navbar item when section is near top of viewport
 function addActiveClass() {
-
     // Get current scroll position
-    let scrollY = window.pageYOffset;
-
+    let scrollY = window.pageYOffset; 
     // Get height of each section
     for (const section of pageSections) {
         let sectionTop = (section.getBoundingClientRect().top + window.pageYOffset) - 100;
         let sectionHeight = section.getBoundingClientRect().height;
+        let sectionId = section.id
 
-     // Add active class to the section that enters the space where current scroll position is
+     // Add active class to navbar item and the section that enters the space where current scroll position is
      // Remove active class otherwise
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            section.classList.add('your-active-class');
+            section.classList.add('active');
+            document.querySelector(`[data-section-id="${sectionId}"]`).classList.add('active');
         } else {
-            section.classList.remove('your-active-class');
+            section.classList.remove('active');
+            document.querySelector(`[data-section-id="${sectionId}"]`).classList.remove('active');
         }
     }
 };
 
-
-// Scroll to anchor ID using scrollTO event
-
-
+// Add scroll to top arrow button
+function addScrollToTopButton() {
+    if (window.scrollY > window.innerHeight) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+}
 
 /**
  * End Main Functions
@@ -89,9 +111,16 @@ function addActiveClass() {
 */
 
 // Build menu
-createNavBar();
+buildNavBar();
 
 // Scroll to section on link click
+scrollToSection();
 
-// Set sections as active
+// Set section and navbar item as 'active'
 window.addEventListener('scroll', addActiveClass);
+
+// Listen for a scroll event to display scroll to top arrow button
+document.addEventListener('scroll', addScrollToTopButton);
+
+// Add click event to scroll to top arrow button
+scrollToTopBtn.addEventListener('click', scrollToTop);
